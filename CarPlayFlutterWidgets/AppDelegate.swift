b3ffc73f28343170d28ec982ed37c38b6fc83fe2
Foti Dim <fdimanidis@gmail.com>
2021-11-24 18:32:18 +0100

    Add CarPlay support

--- CarPlayFlutterWidgets/AppDelegate.swift
+++ CarPlayFlutterWidgets/AppDelegate.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import CarPlay
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, CPApplicationDelegate, CPMapTemplateDelegate {
+    
+    var carPlayWindow: CPWindow?
+    var interfaceController: CPInterfaceController?
+    var mapTemplate: CPMapTemplate = {
+        let mapTemplate = CPMapTemplate()
+        
+        let searchBarButton = CPBarButton(type: .text)
+        searchBarButton.title = "     +     "
+        mapTemplate.leadingNavigationBarButtons = [searchBarButton]
+        
+        return mapTemplate
+    }()
+    
+    func application(_ application: UIApplication, didConnectCarInterfaceController interfaceController: CPInterfaceController, to window: CPWindow) {
+        self.interfaceController = interfaceController
+        self.carPlayWindow = window
+        
+        mapTemplate.mapDelegate = self
+        interfaceController.setRootTemplate(mapTemplate, animated: true)
+        window.rootViewController = UIViewController()
+    }
+    
+    func application(_ application: UIApplication, didDisconnectCarInterfaceController interfaceController: CPInterfaceController, from window: CPWindow) {
+        // Not used
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -32,7 +57,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
